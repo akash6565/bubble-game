16a1bfce8cc629763fb0f10a89290ddcb083d49d
akash6565 <103556222+akash6565@users.noreply.github.com>
2023-09-16 16:10:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+function increScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+    var clutter = "";
+
+for(var i = 1; i<=156; i++){
+    var rn = Math.floor(Math.random()*10);
+    clutter += `<div class="bubble"> ${rn} </div>`;
+}
+
+document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+
+function runTimer() {
+    var timerint = setInterval(function () {
+        if (timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1> GAME OVER </h1>`;
+        }
+    }, 1000);
+}
+document.querySelector("#pbtm").addEventListener("click",function (dets) {
+    var clikednum = Number(dets.target.textContent);
+    if(clikednum == hitrn){
+        increScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+
+runTimer();
+makeBubble();
+getNewHit();
